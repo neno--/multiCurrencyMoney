adbd421eb56b4fc4c25d31c84e49c881b2ba3ac2
nenad <neno.nenadovic@gmail.com>
2020-07-17 13:14:21 +0200

    push currency method to superclass

--- src/main/java/com/github/nenomm/mcm/Franc.java
+++ src/main/java/com/github/nenomm/mcm/Franc.java
@@ -4,9 +4,4 @@ public class Franc extends Money {
     Franc(int amount, String currency) {
         super(amount, currency);
     }
-
-    @Override
-    String currency() {
-        return currency;
-    }
 }
