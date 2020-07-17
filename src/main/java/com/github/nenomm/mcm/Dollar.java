e63f5c281c092609e89b019bc78b07cf842915b1
nenad <neno.nenadovic@gmail.com>
2020-07-17 10:57:35 +0200

    introduce generalized Money

--- src/main/java/com/github/nenomm/mcm/Dollar.java
+++ src/main/java/com/github/nenomm/mcm/Dollar.java
@@ -1,21 +1,11 @@
 package com.github.nenomm.mcm;
 
-public class Dollar {
-
-    int amount;
-
+public class Dollar extends Money {
     Dollar(int amount) {
         this.amount = amount;
     }
 
     Dollar times(int multiplier) {
         return new Dollar(amount * multiplier);
-    }
-
-
-    @Override
-    public boolean equals(Object obj) {
-        Dollar dollar = (Dollar) obj;
-        return amount == dollar.amount;
     }
 }
--- src/main/java/com/github/nenomm/mcm/Franc.java
+++ src/main/java/com/github/nenomm/mcm/Franc.java
@@ -1,21 +1,11 @@
 package com.github.nenomm.mcm;
 
-public class Franc {
-
-    int amount;
-
+public class Franc extends Money {
     Franc(int amount) {
         this.amount = amount;
     }
 
     Franc times(int multiplier) {
         return new Franc(amount * multiplier);
-    }
-
-
-    @Override
-    public boolean equals(Object obj) {
-        Franc dollar = (Franc) obj;
-        return amount == dollar.amount;
     }
 }
