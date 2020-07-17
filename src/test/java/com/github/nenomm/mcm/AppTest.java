6aca26bfdec5183034d53dae7bdb949b2f962628
nenad <neno.nenadovic@gmail.com>
2020-07-17 10:40:30 +0200

    add equality

--- src/test/java/com/github/nenomm/mcm/AppTest.java
+++ src/test/java/com/github/nenomm/mcm/AppTest.java
@@ -2,7 +2,7 @@ package com.github.nenomm.mcm;
 
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 public class AppTest {
     @Test
@@ -12,5 +12,11 @@ public class AppTest {
         assertEquals(10, product.amount);
         product = five.times(3);
         assertEquals(15, product.amount);
+    }
+
+    @Test
+    public void testEquality() {
+        assertTrue(new Dollar(5).equals(new Dollar(5)));
+        assertFalse(new Dollar(5).equals(new Dollar(6)));
     }
 }
